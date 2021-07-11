7b731a45bf0c52a5e2358471e6a551d3172c5172
VaradiTamas <61427905+VaradiTamas@users.noreply.github.com>
2021-07-11 16:24:47 +0200

    header styling
    -added telephone icon -> on click: tel.num. and email addr. appears
    
    -added facebook and instagram icons which will have a reference to the proper pages

--- src/app/layout/app-header/header.component.ts
+++ src/app/layout/app-header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {PopupTelephoneDialogComponent} from "./popup-telephone-dialog/popup-telephone-dialog.component";
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  dialogIsOpened = false;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    if(!this.dialogIsOpened){
+      let dialogRef = this.dialog.open(PopupTelephoneDialogComponent, { panelClass: 'telephone-dialog-container' });
+      this.dialogIsOpened = true;
+      dialogRef.afterClosed().subscribe(result => {
+        this.dialogIsOpened = false;
+      });
+    }
+    else {
+      this.dialogIsOpened = false;
+      this.dialog.closeAll();
+    }
   }
 
 }
---
+++ src/app/layout/app-header/popup-telephone-dialog/popup-telephone-dialog.component.ts
@@ -0,0 +1,15 @@
+import {Component, Inject, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-popup-telephone-dialog',
+  templateUrl: './popup-telephone-dialog.component.html',
+  styleUrls: ['./popup-telephone-dialog.component.css']
+})
+export class PopupTelephoneDialogComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
