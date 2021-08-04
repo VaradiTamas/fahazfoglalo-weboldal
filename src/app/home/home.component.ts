b47f0b09ddbd2d76000324d5cc2a7774a6779f59
VaradiTamas <61427905+VaradiTamas@users.noreply.github.com>
2021-08-04 22:23:06 +0200

    animations half ready
    Animations work almost perfectly:
    -img swipes
    -header text swipes
    -logo scaling
    
    TODO:
    -change images
    -darkening at the end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {state, style, trigger, transition, animate, keyframes} from "@angular/animations";
+import {state, style, trigger, transition, animate, keyframes, group} from "@angular/animations";
 
 @Component({
   selector: 'app-home',
@@ -7,221 +7,182 @@ import {state, style, trigger, transition, animate, keyframes} from "@angular/an
   styleUrls: ['./home.component.css'],
   animations: [
     trigger('fade', [
-      state('first', style({ filter: 'blur(0px)'})),
-      state('second', style({ filter: 'blur(0px)' })),
-      state('third', style({ filter: 'blur(0px)' })),
+      state('first', style({ })),
+      state('second', style({ })),
+      state('third', style({ })),
+      state('fourth', style({ })),
       transition('void => first', [
         style({
-          transform: 'translateX(0px)'
+          transform: 'translateY(-2000px)',
+          filter: 'blur(8px)'
         }),
-        animate(1000)
+        animate(600, style({
+          transform: 'translateY(0px)',
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(0px)',
+          transform: 'scale(1.2)',
+        })),
+        animate(1200, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
       transition('first => second', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateX(-2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            transform: 'scale(1.2)',
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+        style({
+          transform: 'translateX(-2000px)',
+          filter: 'blur(8px)'
+        }),
+        animate(600, style({
+          transform: 'translateX(0px)',
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(0px)',
+          transform: 'scale(1.2)',
+        })),
+        animate(1200, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
       transition('second => third', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateX(2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            transform: 'scale(1.2)',
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+        style({
+          transform: 'translateX(2000px)',
+          filter: 'blur(8px)'
+        }),
+        animate(600, style({
+          transform: 'translateX(0px)',
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(0px)',
+          transform: 'scale(1.2)',
+        })),
+        animate(1200, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
-      transition('third => first', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateY(-2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateY(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            transform: 'scale(1.2)',
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+      transition('third => fourth', [
+        style({
+          transform: 'scale(1.2)',
+        }),
+        animate(2000, style({
+          transform: 'scale(1.3)'
+        })),
+        animate(1000, style({
+          transform: 'scale(1.3)',
+        }))
+      ]),
+      transition('fourth => first', [
+        style({
+          transform: 'translateY(-2000px)',
+          filter: 'blur(8px)'
+        }),
+        animate(600, style({
+          transform: 'translateY(0px)',
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(6px)'
+        })),
+        animate(600, style({
+          filter: 'blur(0px)',
+          transform: 'scale(1.2)',
+        })),
+        animate(1200, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
     ]),
     trigger('titleChange', [
       state('first', style({ })),
       state('second', style({ })),
       state('third', style({ })),
+      state('fourth', style({ })),
       transition('void => first', [
         style({
-          transform: 'translateX(0px)'
+          transform: 'translateY(-2000px)'
         }),
-        animate(1000)
+        animate(600, style({
+          transform: 'translateY(0px)'
+        })),
+        animate(600),
+        animate(600, style({
+          transform: 'scale(1.2)',
+        })),
+        animate(1199, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
       transition('first => second', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateX(-2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+        style({
+          transform: 'translateX(-2000px)'
+        }),
+        animate(600, style({
+          transform: 'translateX(0px)'
+        })),
+        animate(600),
+        animate(600, style({
+          transform: 'scale(1.2)',
+        })),
+        animate(1199, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
       transition('second => third', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateX(2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            transform: 'translateX(0px)',
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+        style({
+          transform: 'translateX(2000px)'
+        }),
+        animate(600, style({
+          transform: 'translateX(0px)'
+        })),
+        animate(600),
+        animate(600, style({
+          transform: 'scale(1.2)',
+        })),
+        animate(1199, style({
+          transform: 'scale(1.2)'
+        }))
       ]),
-      transition('third => first', [
-        animate(3000, keyframes([
-          style({
-            transform: 'translateY(-2000px)',
-            filter: 'blur(8px)',
-            offset: 0
-          }),
-          style({
-            transform: 'translateY(0px)',
-            filter: 'blur(6px)',
-            offset: 0.2
-          }),
-          style({
-            filter: 'blur(6px)',
-            offset: 0.4
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 0.6
-          }),
-          style({
-            filter: 'blur(0px)',
-            offset: 1
-          })
-        ]))
+      transition('third => fourth', [
+        style({
+          transform: 'scale(1.2)'
+        }),
+        animate(2999, style({
+          transform: 'scale(1.4)',
+        })),
       ]),
+      transition('fourth => first', [
+        style({
+          transform: 'translateY(-2000px)'
+        }),
+        animate(600, style({
+          transform: 'translateY(0px)'
+        })),
+        animate(600),
+        animate(600, style({
+          transform: 'scale(1.2)',
+        })),
+        animate(1199, style({
+          transform: 'scale(1.2)'
+        }))
+      ])
     ])
   ]
-//     trigger('divState', [
-//       state('first', style({
-//         opacity: 1,
-//         transform: 'translateX(0)'
-//       })),
-//       transition('void => *', [
-//         style({
-//           opacity: 0,
-//           transform: 'translateX(-100px)'
-//         }),
-//         animate(2000)
-//       ])
-//     ]),
-//     trigger('secondState', [
-//       state('second', style({
-//         opacity: 1,
-//         transform: 'translateX(0)'
-//       })),
-//       transition('void => *', [
-//         style({
-//           opacity: 0,
-//           transform: 'translateX(100px)'
-//         }),
-//         animate(2000)
-//       ])
-//     ]),
-//   ]
 })
 
 export class HomeComponent implements OnInit {
   state = 'first';
-  titleState = 'first';
+  isLogoVisible = false;
   headerText = 'ELSO';
   counter = 0;
   imageSource = '';
@@ -238,10 +199,10 @@ export class HomeComponent implements OnInit {
   toggleState() {
     switch(this.counter){
       case 0: {
+        this.isLogoVisible = false;
         this.previousImageSource = this.imgSrc3;
         this.imageSource = this.imgSrc1;
         this.state = 'first';
-        this.titleState = 'first';
         this.headerText = 'ELSO';
         break;
       }
@@ -249,7 +210,6 @@ export class HomeComponent implements OnInit {
         this.previousImageSource = this.imgSrc1;
         this.imageSource = this.imgSrc2;
         this.state = 'second';
-        this.titleState = 'second';
         this.headerText = 'MASODIK';
         break;
       }
@@ -257,15 +217,18 @@ export class HomeComponent implements OnInit {
         this.previousImageSource = this.imgSrc2;
         this.imageSource = this.imgSrc3;
         this.state = 'third';
-        this.titleState = 'third';
         this.headerText = 'HARMADIK';
         break;
+      }
+      case 3: {
+        this.isLogoVisible = true;
+        this.state = 'fourth';
       }
     }
   }
 
   onDone($event) {
-    if(this.counter === 2){
+    if(this.counter === 3){
       this.counter = 0;
     } else {
       this.counter++;
