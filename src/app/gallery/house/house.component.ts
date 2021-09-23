426dacc51f7fd9249c14e9e274f3b6673142a5a7
Tamás Váradi <tamas.varadi.ext@siemens.com>
2021-09-23 20:31:06 +0200

    gallery styling
    It works well.
    To do in future:
    - styling
    - make it responsive
    - close button navigates us to the home page, should navigate to the current gallery component
    - also give the close button a nice style (maybe an x icon)

--- src/app/gallery/house/house.component.ts
+++ src/app/gallery/house/house.component.ts
@@ -9,10 +9,12 @@ import {Image} from '../../gallery-components/image.interface';
 export class HouseComponent implements OnInit {
 
   public images = [
-    {path: 'assets/park/park1.jpg'},
-    {path: 'assets/park/park2.jpg'},
-    {path: 'assets/park/park3.jpg'},
-    {path: 'assets/park/park4.jpg'},
+    {path: 'assets/house/house1.png'},
+    {path: 'assets/house/house2.jpg'},
+    {path: 'assets/house/house3.jpg'},
+    {path: 'assets/house/house4.jpg'},
+    {path: 'assets/house/house5.jpg'},
+    {path: 'assets/house/house6.jpg'},
   ] as Array<Image>;
 
   constructor() { }
