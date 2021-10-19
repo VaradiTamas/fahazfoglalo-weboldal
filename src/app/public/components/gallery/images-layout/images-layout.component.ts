275283c383bb2a29fbec1e3eed7eef551a0ba0f2
Tamás Váradi <tamas.varadi.ext@siemens.com>
2021-10-19 21:03:06 +0200

    before calendar logic update

---
+++ src/app/public/components/gallery/images-layout/images-layout.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Image} from '../image.interface';
+
+@Component({
+  selector: 'app-images-layout',
+  templateUrl: './images-layout.component.html',
+  styleUrls: ['./images-layout.component.css']
+})
+export class ImagesLayoutComponent implements OnInit {
+
+  public selectedImage?: number = null;
+
+  @Input() images: Array<Image>;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
