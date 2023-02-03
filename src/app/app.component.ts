8c271e31f7f0da6601f6a8acbe353e0fcf1bcadd
Tamas Varadi <varadi.thomas@gmail.com>
2023-02-03 18:23:30 +0100

    redesigns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from "./admin/auth/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './admin/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit{
 
   constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.authService.autoAuthUser();
   }
 }
